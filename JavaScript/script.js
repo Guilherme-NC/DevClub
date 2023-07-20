b3b5868eefe44e708239ef9b2a6ee9f35ca8fe89
GuilhermeNC <guilherme.caires.santos@gmail.com>
2023-07-19 21:38:47 -0300

    ADD alert with JS para the home page by app

---
+++ JavaScript/script.js
@@ -0,0 +1,19 @@
+const users = [{
+    Nome: 'Guilheme',
+    Age: 20,
+    nacionality: 'Brazil',
+}]
+
+
+if (users[0].nacionality == 'Brazil') {
+    console.log('Usuario Cadastrado com suceso')
+} else {
+    console.log('Voçê não è brasileiro')
+}
+
+function button() {
+    console.log(2 * 2)
+
+}
+
+button()
